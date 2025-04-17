506b96400ec823b4212888b888c3ca7bf83c95be
Erik Kling <kling.erik@icloud.com>
2025-04-17 15:53:10 +0200

    Add some comments to make the code readable

--- game_logic.py
+++ game_logic.py
@@ -10,31 +10,41 @@ def get_random_word():
 
 
 def play_game():
+    """Plays the game."""
     secret_word = get_random_word()
     print("Welcome to Snowman Meltdown!")
 
     mistakes = 0
     guessed_letters = []
     while True:
+        # Checks if max mistakes have been made
         if mistakes == 4:
             print(f"Game Over! The word was: {secret_word}")
             get_snowman_stage(mistakes)
             ask_to_play_again()
-        else: 
+        else:
+            # show current snowman stage
             get_snowman_stage(mistakes)
+
+            # DIsplay the word with guessed letters and underscores
             print("Word: ", end="")
             for char in secret_word:
                 if char in guessed_letters:
                     print(char, end=" ")
                 else:
                     print("_", end=" ")
+
+            # Get user input
             guess = input("\n\nGuess a letter: ").lower()
+
+            # Check if the guess is valid
             if len(guess) != 1 or not guess.isalpha():
                 print("Invalid input. Please enter a single letter.")
                 continue
+
             guessed_letters.append(guess)
 
-
+            # Check if the guess is in the secret word
             if guess not in secret_word:
                 mistakes += 1
             
@@ -45,19 +55,21 @@ def play_game():
                 all_guessed = False
                 break
 
+        # If word is fully guessed -> victory!
         if all_guessed:
             print(f"\nCongratulations, you saved the snowman! The word was: {secret_word}")
             get_snowman_stage(mistakes)
             ask_to_play_again()
 
 def ask_to_play_again():
+    """ Ask the user if they want to play again."""
     while True:
         try:
             play_again = input("Do you wanna play again (yes/no): ").lower()
             print("")
             if play_again == "yes":
                 play_game()
-                break  # Wichtig, damit die Schleife nicht ewig weiterläuft
+                break  # exit the loop
             elif play_again == "no":
                 print("Thanks for playing!")
                 exit()
