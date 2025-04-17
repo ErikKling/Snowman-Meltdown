599e166e45a4db07edd2724a5fc8ea3488305732
Erik Kling <kling.erik@icloud.com>
2025-04-17 15:09:05 +0200

    Move STAGES and get_snowman_stage to ascii_art.py

--- snowman.py
+++ snowman.py
@@ -1,34 +1,5 @@
 import random
-
-# Snowman ASCII Art stages
-STAGES = [
-     # Stage 0: Full snowman
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
-     ( : ) 
-     """,
-     # Stage 1: Bottom part starts melting
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
-     """,
-     # Stage 2: Only the head remains
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     """,
-     # Stage 3: Snowman completely melted
-     """
-      ___  
-     /___\\ 
-     """
- ]
+from ascii_art import get_snowman_stage
 
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
@@ -37,8 +8,6 @@ def get_random_word():
     """Selects a random word from the list."""
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
-def get_snowman_stage(stage):
-    print(STAGES[stage])
 
 def play_game():
     secret_word = get_random_word()
